c4f28d30053b7b91feebf38a53fc779c3ccb03dd
GurudeepNaik <gurudeepnaik1999@gmail.com>
2022-11-15 19:44:02 +0530

    make it better

--- routes/router.js
+++ routes/router.js
@@ -6,11 +6,11 @@ const deleteUser=require("../controller/deleteUser");
 const updateUser=require("../controller/updateUser");
 const getSortedUser=require("../controller/getSortedUser");
 
-route.get("/sorted", getSortedUser);//GET -> http://localhost:7000/user/sorted?public_repos=true
-route.get("/search", searchUserDetails); //GET -> http://localhost:7000/user/search?login=GurudeepNaik
-route.post("/:userName" , addUserDetails); //POST -> http://localhost:7000/user/GurudeepNaik
-route.delete("/:userName", deleteUser);//DELETE -> http://localhost:7000/user/GurudeepNaik
-route.put("/:userName", updateUser); //PUT -> http://localhost:7000/user/GurudeepNaik
+route.get("/sorted", getSortedUser);//GET -> https://fivec-backend.herokuapp.com/user/sorted?public_repos=true
+route.get("/search", searchUserDetails); //GET -> https://fivec-backend.herokuapp.com/user/search?login=GurudeepNaik
+route.post("/:userName" , addUserDetails); //POST ->  https://fivec-backend.herokuapp.com/user/GurudeepNaik
+route.delete("/:userName", deleteUser);//DELETE -> https://fivec-backend.herokuapp.com/user/GurudeepNaik
+route.put("/:userName", updateUser); //PUT -> https://fivec-backend.herokuapp.com/user/GurudeepNaik
 
 route.get("*", (req, res) => {
   res.status(404).json({
